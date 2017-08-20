b8d77bf651bf84518db3328eaf7348740dd39ba7
Guy John <guy@rumblesan.com>
2017-08-20 21:29:40 +0100

    remove debug logging

--- src/jpeg/quantGlitcher.js
+++ src/jpeg/quantGlitcher.js
@@ -8,7 +8,6 @@ export function randomQuantGlitch(imageData, sections) {
       for (p = s.position + 2; p < s.position + s.size; p += 1) {
         if (Math.random() < 0.1) {
           g = Math.floor(Math.random() * 253 + 1);
-          console.log(imageData[p], g);
           imageData[p] = g;
         }
       }
