838f89828c64d796812e611a1188a283024bf8ad
Guy John <guy@rumblesan.com>
2017-08-22 21:28:07 +0100

    Add jpeg structure and copy function

---
+++ src/jpeg/index.js
@@ -0,0 +1,15 @@
+import decodeJpeg from './decoder';
+
+export function Jpeg(data) {
+  return {
+    type: 'jpeg',
+    data,
+    sections: decodeJpeg(data),
+  };
+}
+
+export function copy({ data }) {
+  // results in reparsing the new jpeg
+  // may or may not be a bad idea
+  return Jpeg(data.subarray());
+}
