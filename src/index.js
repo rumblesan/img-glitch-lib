07939d4c2b11470eadf58e510a5f7c7b768296d9
Guy John <guy@rumblesan.com>
2017-09-07 16:39:17 +0100

    Export jpeg constructor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import decodeJpeg from 'jpeg/decoder';
 import { randomQuantGlitch } from 'jpeg/quantGlitcher';
+import * as Jpeg from 'jpeg';
 
 import * as utils from 'utils';
 
-export { decodeJpeg, randomQuantGlitch, utils };
+export { decodeJpeg, randomQuantGlitch, utils, Jpeg };
