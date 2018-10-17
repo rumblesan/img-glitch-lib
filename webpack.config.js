7b46d7cd25e1ad4905e3b4e2ae4520945aa2937e
Guy John <guy@rumblesan.com>
2018-10-17 21:47:21 +0100

    Updating build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,38 @@
 /* global __dirname, require, module*/
 
-const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
-const env = require('yargs').argv.env; // use --env with webpack 2
 
-let libraryName = 'glitchlib';
+const libraryName = 'glitchlib';
 
-let plugins = [],
-  outputFile;
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
-
-const config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      { test: /\.pegjs$/, loader: 'pegjs-loader' },
-    ],
-  },
-  resolveLoader: {
-    modules: ['./webpack', './node_modules'],
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')],
-    extensions: ['.json', '.js', '.pegjs'],
-  },
-  plugins: plugins,
+module.exports = (env, argv) => {
+  const outputName = `${libraryName}${
+    argv && argv.mode === 'production' ? '.min.js' : '.js'
+  }`;
+  return {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    mode: 'production',
+    devtool: 'eval-source-map',
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: outputName,
+      library: libraryName,
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/env']],
+          },
+        },
+      ],
+    },
+    resolve: {
+      modules: [path.resolve('./node_modules'), path.resolve('./src')],
+      extensions: ['.json', '.js'],
+    },
+  };
 };
-
-module.exports = config;
